dd2f366e54281841792514ac9985bbfacc992d26
Yuyang He <Yuyang He@YuyangHe>
2015-08-16 01:24:53 +0800

    The first project I commit.

---
+++ Hello World/src/com/yuyang/he/test/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.yuyang.he.test;
+
+/**
+ * Test project for BitBucket.
+ * @author Yuyang He
+ * @date 1:22:00 AM, Aug 16, 2015
+ * @version 1.0
+ */
+public class HelloWorld {
+
+	/**
+	 * Main function.
+	 * @param args User typed parameters.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
